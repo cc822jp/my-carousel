ef8b93531c27f3567459f0333008d96ed6c6e03a
Tatsuya Sasaki <sasaki@crystalcode.jp>
2019-12-03 21:06:48 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <carousel :per-page="1">
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/Carousel';
+import Slide from './components/Slide';
+
+export default {
+  name: 'app',
+  components: {
+    Carousel,
+    Slide
+  }
+};
+</script>
